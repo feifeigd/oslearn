2acf28cdde6255c903ee25456d64c5119b5c44bb
luofei@xtgames.cn <luofei@xtgames.cn>
2022-11-17 18:34:16 +0800

    获取物理内存的大小

--- build.sh
+++ build.sh
@@ -37,4 +37,4 @@ echo 写入MBR
 dd if=bin/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 
 echo 写入LOADER
-dd if=bin/loader.bin of=hd60M.img bs=512 count=1  seek=2 conv=notrunc
+dd if=bin/loader.bin of=hd60M.img bs=512 count=2  seek=2 conv=notrunc
