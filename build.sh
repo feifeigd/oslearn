c5dbe82d149a45e88bc54b947610019fddf7f4c5
luofei@xtgames.cn <luofei@xtgames.cn>
2022-11-24 10:35:35 +0800

    编译内核

--- build.sh
+++ build.sh
@@ -32,9 +32,15 @@ build "Release"
 
 
 echo 'success';
+echo '编译内核'
+gcc -c kernel/main.c && ld main.o -Ttext 0xc0001500 -e main -o bin/kernel.bin
 
 echo 写入MBR
 dd if=bin/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 
 echo 写入LOADER
-dd if=bin/loader.bin of=hd60M.img bs=512 count=2  seek=2 conv=notrunc
+dd if=bin/loader.bin of=hd60M.img bs=512 count=3  seek=2 conv=notrunc
+
+echo 写入 kernel
+dd if=bin/kernel.bin of=hd60M.img bs=512 count=200  seek=9 conv=notrunc
+
