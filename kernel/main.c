784ca05d30f1b8974f7bdd6b8b9e5947bfc4748d
feifeigd <feifeigd@21cn.com>
2022-11-29 00:30:04 +0800

    修复一个bug

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,16 @@
 
 int main(void){
     put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+    put_char('1');
+    put_char('2');
+    put_char('\b');
+    put_char('3');
     while(1);
     return 0;
 }
