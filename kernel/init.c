8dd6c2535e1fb167baa1a94aee9a1d0493ecacba
luofei@xtgames.cn <luofei@xtgames.cn>
2022-12-02 14:00:16 +0800

    timer_init

--- kernel/init.c
+++ kernel/init.c
@@ -1,10 +1,11 @@
 #include "init.h"
 #include "interrupt.h"
-
+#include "timer.h"
 #include <print.h>
 
 // 负责初始化所有模块
 void init_all(void){
     put_str("init_all\n");
     idt_init();// 初始化中断
+    timer_init();   // 初始化 PIT
 }
