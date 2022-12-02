1f755d0a9257f778f35379426d932b659fe5fe96
luofei@xtgames.cn <luofei@xtgames.cn>
2022-12-02 10:15:09 +0800

    stdint

--- lib/stdint.h
+++ lib/stdint.h
@@ -1,4 +1,8 @@
 #pragma once
 
 typedef char            int8_t;
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
 typedef unsigned int    uint32_t;
+typedef unsigned long long    uint64_t;
