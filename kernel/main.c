e82adf62b28081e7411f4e41c34d47906e49b05f
feifeigd <feifeigd@21cn.com>
2022-12-10 01:00:09 +0800

    线程

--- kernel/main.c
+++ kernel/main.c
@@ -1,13 +1,10 @@
-#include <debug.h>
-#include <init.h>
+#include "debug.h"
+#include "global.h"
+#include "init.h"
+#include "memory.h"
 #include <print.h>
 
-void k_thread_a(void* arg){
-    char* para = arg;
-    put_str("run thread:");
-    put_str(para);
-    put_str("\n");
-}
+static void k_thread_a(void* arg);
 
 int main(void){ // b 0x1500
     put_str("I am kernel\n");
@@ -17,10 +14,22 @@ int main(void){ // b 0x1500
     // ASSERT(1 == 2);
     void* addr = get_kernel_pages(3);   // 申请3个内存页, b 0x1546
     put_str("\n    get_kernel_page start vaddr is "); put_int((uint32_t)addr); put_str("\n");
-
     put_str("create thread\n");
-    thread_start("k_thread_a", 31, k_thread_a, "argA");
+    char* arg = "argA";
+    thread_start("k_thread_a", 31, k_thread_a, arg);
 
     while(1);
     return 0;
+}
+
+static void k_thread_a(void* arg){
+    
+    char* para = arg;
+    while (true)
+    {
+        put_str("run thread:");
+        put_str(para);        
+        put_str("\n");
+        //return;
+    }    
 }
