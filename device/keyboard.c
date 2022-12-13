051b57bd54d57f92df6d0c9d21ceb72f9ce4bc02
luofei@xtgames.cn <luofei@xtgames.cn>
2022-12-13 14:54:17 +0800

    intr_keyboard_handler

--- device/keyboard.c
+++ device/keyboard.c
@@ -113,8 +113,81 @@ static char keymap[][2] = {
 
 // 键盘中断处理程序
 static void intr_keyboard_handler(void){
-    uint8_t scancode = inb(KBD_BUF_PORT);
-    put_int(scancode);
+    bool ctrl_down_last = ctrl_status;
+    bool shift_down_last = shift_status;
+    bool caps_lock_last = caps_lock_status;
+
+    bool break_code;
+    uint16_t scancode = inb(KBD_BUF_PORT);
+    // 若扫描码是e0开头的，表示此键的按下将产生多个扫描码
+    // 所以马上结束此次中断处理函数，等待下一个扫描码进来
+    if(0xe0 == scancode){
+        ext_scancode = true; // 打开 e0 标记
+        return;
+    }
+    // 合并扫描码
+    if (ext_scancode)
+    {
+        scancode |= 0xe000;
+        ext_scancode = false;
+    }
+
+    break_code = 0x80 & scancode; 
+    if (break_code) // 按键弹起
+    {
+        // 由于ctrl_r 和 alt_r 的 make_code 和 break_code 都是两字节，所以可用下面的方法取make_code,多字节的扫描码暂不处理
+        uint16_t make_code = scancode & 0xff7f;
+        // 得到其 make_code(按键按下时产生的扫描码)
+
+        // 若是任意以下三个键弹起了，将状态置为false
+        if(ctrl_l_make == make_code || ctrl_r_make == make_code)
+            ctrl_status = false;
+        else if(shift_l_make == make_code || shift_r_make == make_code)
+            shift_status = false;
+        else if(alt_l_make == make_code || alt_r_make == make_code)
+            alt_status = false;
+
+        return;
+    }else if( (scancode > 0 && scancode < 0x3B) || alt_r_make == scancode || ctrl_r_make == scancode){  // 若为通码，只处理数组中定义的键，以及alt_right和ctrl键，全是make_code
+        bool shift = false;
+        if (scancode < 0x0e // '0' ~ '9' '-' '='
+            || scancode == 0x1a || scancode == 0x1b // '[' ']'
+            || scancode == 0x2b || scancode == 0x27 || scancode == 0x28 || scancode == 0x29 // '\\' ';' '\'' '`'
+            || scancode == 0x33 || scancode == 0x34|| scancode == 0x35) // ',' '.' '/'
+        {
+            if(shift_down_last)
+                shift = true;
+        }else{// 默认为字母键
+            if(shift_down_last && caps_lock_last)// 两个键同时按下
+                shift = false; 
+            else if(shift_down_last || caps_lock_last)
+                shift = true;
+            else
+                shift = false;
+        }
+        
+        uint8_t index = scancode &= 0xff;
+        char cur_char = keymap[index][shift];
+        // 只处理ASCII码不为零的键
+        if(cur_char){
+            put_char(cur_char);
+            return;
+        }
+        // 记录本次是否按下了下面几类控制键,供下次键入时判断组合键
+        
+        if(ctrl_l_make == scancode || ctrl_r_make == scancode)
+            ctrl_status = true;
+        else if(shift_l_make == scancode || shift_r_make == scancode)
+            shift_status = true;
+        else if(alt_l_make == scancode || alt_r_make == scancode)
+            alt_status = true;
+        else 
+            caps_lock_status = !caps_lock_status;   // 大写键 反转
+    }else{  
+        put_str("unknown key\n");
+    }    
+    
+    // put_int(scancode);
 }
 
 void keyboard_init(void){
