fd108766ab142075fa4bcdeb4d82ac2e23b1a14e
feifeigd <feifeigd@21cn.com>
2022-12-13 21:54:23 +0800

    GDT 放在  loader 开头

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ echo 写入MBR
 dd if=bin/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 
 echo 写入LOADER
-dd if=bin/loader.bin of=hd60M.img bs=512 count=3  seek=2 conv=notrunc
+dd if=bin/loader.bin of=hd60M.img bs=512 count=4  seek=2 conv=notrunc
 
 echo 写入 kernel
 make
